d38e4d52683721252ac990aba67cf0b881d95367
Alaia Morell <alaia.morell@gmail.com>
2026-03-05 14:36:41 +0000

    UVW write interpolation correction

--- R/write_interpoled_data.R
+++ R/write_interpoled_data.R
@@ -77,13 +77,13 @@ write_interpoled_data <- function(data, start_year, end_year, variable, scenario
                       "uvw",
                       "NO3", "NH3",
                       "Sm_Zooplankton_N", "Lrg_Zoo_N", "Meso_Zoo_N", "Sm_Phyto_N", "Lrg_Phyto_N",
-                      "Oxygen", "Lab_Det_N", "Ref_Det_N", "RDON"),
+                      "Oxygen", "Lab_Det_N", "Ref_Det_N", "DON"),
 
     atlantis.longname = c("Salinity", "Temperature",
                           "uvw",
                           "NO3", "NH3",
                           "SmallZooplankton", "LargeZooplankton", "MesoZooplankton", "Small phytoplankton (dinoflagellates)", "Large phytoplankton (diatoms)",
-                          "Dissolved oxygen", "Labile particulate organic nitrogen", "Refractory particulate organic nitrogen", "Refractory dissolved organic nitrogen"),
+                          "Dissolved oxygen", "Labile particulate organic nitrogen", "Refractory particulate organic nitrogen", "Dissolved organic nitrogen"),
     atlantis.unit = c("g.L-1", "°C",
                           "m^3",
                           "mg N m-3", "mg N m-3",
@@ -108,7 +108,7 @@ write_interpoled_data <- function(data, start_year, end_year, variable, scenario
     time = time = seq(0,dim(data$exchange)[4]*12*60*60-1, 12*60*60)
 
     # Define dimensions
-    z_dim <- ncdf4::ncdim_def("z","layerNum", 1:dim(data$exchange)[1]) # 7 layers
+    z_dim <- ncdf4::ncdim_def("z","layerNum", 0:(dim(data$exchange)[1]-1)) # 7 layers
     b_dim <- ncdf4::ncdim_def("b","boxNum", 0:(dim(data$exchange)[3]-1)) # 89 polygons
     t_dim <- ncdf4::ncdim_def("t",units = paste0("seconds since ",start_year,"-01-01"), time, unlim = T)
     dest_dim <- ncdf4::ncdim_def("dest", "Nb max of destinaions", 1:dim(data$exchange)[2])
@@ -136,8 +136,8 @@ write_interpoled_data <- function(data, start_year, end_year, variable, scenario
     ncdf4::ncvar_put(nc, z_var, 1:(dim(data$exchange)[1]))
     ncdf4::ncvar_put(nc, b_var, 0:(dim(data$exchange)[3]-1))
     ncdf4::ncvar_put(nc, exchange_var, data$exchange, start = c(1,1,1,1), count = c(dim(data$exchange)[2], dim(data$exchange)[1],dim(data$exchange)[3], dim(data$exchange)[4]))
-    ncdf4::ncvar_put(nc, dest_b_var,   data$dest_b, start   = c(1,1,1,1), count = c(dim(data$exchange)[2], dim(data$exchange)[1],dim(data$exchange)[3], dim(data$exchange)[4]))
-    ncdf4::ncvar_put(nc, dest_k_var,   data$dest_k, start   = c(1,1,1,1), count = c(dim(data$exchange)[2],dim(data$exchange)[1],dim(data$exchange)[3], dim(data$exchange)[4]))
+    ncdf4::ncvar_put(nc, dest_b_var,   data$dest_b,   start = c(1,1,1,1), count = c(dim(data$exchange)[2], dim(data$exchange)[1],dim(data$exchange)[3], dim(data$exchange)[4]))
+    ncdf4::ncvar_put(nc, dest_k_var,   data$dest_k,   start = c(1,1,1,1), count = c(dim(data$exchange)[2], dim(data$exchange)[1],dim(data$exchange)[3], dim(data$exchange)[4]))
 
     # Add dt attribute to t variable
     ncdf4::ncatt_put(nc, "t", "dt", 43200.0)
